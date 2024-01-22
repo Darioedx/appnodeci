1e8bc1bf90bc770239f9d76e20cb4fb6ddd508d9
Darioedx <dariovicentealmiron@gmail.com>
2024-01-22 09:25:40 -0300

    ‘initial’

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+app.listen(port, () => {
+console.log(`Servidor escuchando en http://localhost:${port}`);
+})
